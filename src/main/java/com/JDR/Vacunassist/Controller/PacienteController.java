cbe27ae40d9a5f3157dee2520e77d31bf5469bb2
Juan Cruz Gutierrez <juan.cruz.gutierrez.saravia@gmail.com>
2022-06-22 23:26:39 -0300

    Jeje

--- src/main/java/com/JDR/Vacunassist/Controller/PacienteController.java
+++ src/main/java/com/JDR/Vacunassist/Controller/PacienteController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -113,4 +114,8 @@ public class PacienteController {
 		return ResponseEntity.ok(pacienteService.cargarPaciente(pacienteRequest));
 	}
 	
+	@DeleteMapping("/deletePaciente")
+	public ResponseEntity<Boolean> deletePaciente(@RequestParam("pacienteId") Integer pacienteId) throws ResourceNotFoundException{
+		return ResponseEntity.ok(pacienteService.deletePaciente(pacienteId));
+	}
 }
